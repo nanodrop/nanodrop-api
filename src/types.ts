ef4db81450510a4418b92ecf1da0e0aec4f18137
anarkrypto <anarkrypto@gmail.com>
2023-09-17 13:54:17 -0300

    🗃️ Add D1, create drops and ip_info table

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ export type Bindings = {
     ENVIRONMENT: 'development' | 'production'
     HONO_QUEUE: Queue<any>
     DURABLE_OBJECT: DurableObjectNamespace
+    DB: D1Database
     RPC_URLS: string
     WORKER_URLS: string
     PRIVATE_KEY: string
